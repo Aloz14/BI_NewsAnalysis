0d54c2bd9192c66c44d0bdb5e438b4451584b314
Aloz14 <2151617@tongji.edu.cn>
2024-05-30 02:12:09 +0800

    更改了存储、读取日志的模式

--- src/LogGenerate/TimeSimulation.py
+++ src/LogGenerate/TimeSimulation.py
@@ -28,21 +28,35 @@ if __name__ == '__main__':
     # remote_log = LogUploader.LogUploader('localhost', 22, 'root', 'root')
     # remote_log.connect()
 
-    index = 0
-    start_time = datetime(2019, 6, 15, 10, 19, 50)  # 6/15/2019 10:19:50 PM
-    current_time = start_time
+    # 读取index.txt文件
+    if os.path.exists('index.txt'):
+        with open('index.txt', 'r') as file:
+            index = int(file.readline())
+            current_time = datetime.strptime(file.readline(), "%m/%d/%Y %I:%M:%S %p")
+    else:
+        index = 0
+        # 6/15/2019 10:19:50 PM
+        current_time = datetime(2019, 6, 15, 10, 19, 50)
     while True:
         print(f"\rCurrent time: {current_time}", end='', flush=True)
         current_time += timedelta(seconds=1)
+        # 把时间和index写入到文件
+        with open('index.txt', 'w') as file:
+            file.write(str(index) + '\n')
+            file.write(current_time.strftime("%m/%d/%Y %I:%M:%S %p"))
+
         if index < len(data):
             if current_time >= datetime.strptime(data['end'][index], "%m/%d/%Y %I:%M:%S %p"):
 
                 # 日志生成
                 # 选择是生成到本地日志文件还是发送到服务器
                 # remote_log.write_data_to_file(data.loc[index], '/root/log/impression.log')
-                with open('../../log/impression.log', 'a') as file:
+                with open('../../log/impression-' + str(time.time()) + '.log', 'a') as file:
                     file.write(data.loc[index].to_json() + '\n')
 
                 index += 1
+
+
+
         time.sleep(1)
 
