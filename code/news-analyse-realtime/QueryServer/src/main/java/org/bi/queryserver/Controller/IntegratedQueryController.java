9190099d5768996873a1f7135eac089ad6558558
Aloz14 <2151617@tongji.edu.cn>
2024-06-16 16:13:11 +0800

    简化数据结构

--- code/news-analyse-realtime/QueryServer/src/main/java/org/bi/queryserver/Controller/IntegratedQueryController.java
+++ code/news-analyse-realtime/QueryServer/src/main/java/org/bi/queryserver/Controller/IntegratedQueryController.java
@@ -1,10 +1,8 @@
 package org.bi.queryserver.Controller;
 
 import org.bi.queryserver.Domain.Clicks;
-import org.bi.queryserver.Domain.ReceiveDS;
 import org.bi.queryserver.Service.impl.IntegratedQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
